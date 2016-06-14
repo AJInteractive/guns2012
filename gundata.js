3f9270c1ed1aefdfa16a41053628a71754e5a15a
Web <Web@admins-imac-04.aljazeera.tv>
2016-06-14 12:34:15 +0300

    Guns 2012 data

---
+++ gundata.js
@@ -0,0 +1,12 @@
+
+var driving = [
+
+  {side: "left", year: 2002, miles: 8454322, gas:  204967},
+  {side: "right", year: 2003, miles: 8481588, gas: 193420},
+  {side: "left", year: 2004, miles: 8687671, gas: 216006},
+  {side: "bottom", year: 2005, miles: 8952945, gas: 225813},
+  {side: "top", year: 2006, miles: 10036933, gas: 248744},
+  {side: "top", year: 2007, miles: 11177335, gas: 287836},
+  {side: "left", year: 2008, miles: 12709023, gas: 312662},
+  {side: "right", year: 2009, miles: 14033824, gas: 452692}
+];
